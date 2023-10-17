6548df325497ba83cb6fc212d1304041aac19746
b13nmaN <blenmanjoel@gmail.com>
2023-10-17 00:54:44 -0400

    added new classes for square and a empty array

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,11 +4,39 @@ document.addEventListener("DOMContentLoaded", function () {
 
   let squares = board.children;
 
+  //initialize an empty array to keep track of the state of the game 
+  //after each move
+  let gameStatus = [];
+
+
   console.log("This is the board", squares);
 
   //add className = square to all squares inside the board
   for (let i = 0; i < squares.length; i++) {
     squares[i].classList.add("square");
-    
+
   }
+
+  //Add an X or O to a square when clicked
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("click", function () {
+      if (this.textContent === "") {
+        // add class square.X
+        this.classList.add("X");
+        this.textContent = "X";
+        console.log("This is the game status", gameStatus);
+        //append move to array
+        gameStatus.push("X");
+      } else if (this.textContent === "X") {
+        // add class square.O
+        this.classList.add("O");
+        this.textContent = "O";
+        //append move to array
+        gameStatus.push("O");
+      } else {
+        this.textContent = "";
+      }
+    });
+  }
+
 });
